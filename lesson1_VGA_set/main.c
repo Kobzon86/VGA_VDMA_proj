dc9f32486a5e06a068c98678752f6732cbad9ffa
Maksim Ananev <65023680+Kobzon86@users.noreply.github.com>
2025-04-21 14:59:08 +0400

    Update main.c

--- lesson1_VGA_set/main.c
+++ lesson1_VGA_set/main.c
@@ -1,38 +1,30 @@
+/******************************************************************************
+* Copyright (C) 2023 Advanced Micro Devices, Inc. All Rights Reserved.
+* SPDX-License-Identifier: MIT
+******************************************************************************/
+/*
+ * helloworld.c: simple test application
+ *
+ * This application configures UART 16550 to baud rate 9600.
+ * PS7 UART (Zynq) is not initialized by this application, since
+ * bootrom/bsp configures it to baud rate 115200
+ *
+ * ------------------------------------------------
+ * | UART TYPE   BAUD RATE                        |
+ * ------------------------------------------------
+ *   uartns550   9600
+ *   uartlite    Configurable only in HW design
+ *   ps7_uart    115200 (configured by bootrom/bsp)
+ */
+
 #include <stdio.h>
 #include "platform.h"
 #include "xil_printf.h"
 
 #include "xparameters.h"
 
+#include "xv_tpg.h"
 #include "xvtc.h"
-#include "xil_cache.h"
-
-#define PATTERN_GREY
-#define PATTERN_COL_BARS
-
-#define RED   0x00e00000
-#define GREEN 0x0000e000
-#define BLUE  0x000000e0
-
-#define RED_HALF   0x00600000
-#define GREEN_HALF 0x00006000
-#define BLUE_HALF  0x00000060
-
-#define RED_EIGHTS   0x00200000
-#define GREEN_EIGHTS 0x00002000
-#define BLUE_EIGHTS  0x00000020
-
-
-
-#define BLUE_YUYV		0x6B1DFF1D
-#define RED_YUYV		0xFF4C544C
-#define GREEN_YUYV		0x15952B95
-#define MAGENTA_YUYV	0xEA69D469
-#define YELLOW_YUYV		0X94FF00FF
-#define CYAN_YUYV		0x00B2ABB2
-#define WHITE_YUYV		0x80FF80FF
-#define BLACK_YUYV		0x80008000
-
 
 #define WIDTH 1280
 #define HEIGHT 720
@@ -43,6 +35,7 @@ int main()
 
 
     int Status;
+    XV_tpg tpg_inst;
     XVtc VtcInst;
 
 
@@ -66,138 +59,43 @@ int main()
 
 
 
+    print("VPG init started\n\r");
+    Status = XV_tpg_Initialize(&tpg_inst, XPAR_V_TPG_0_BASEADDR);
+    if(Status!= XST_SUCCESS)
+    {
+            xil_printf("TPG configuration failed\r\n");
+            return(XST_FAILURE);
+    }
+
+
+
+    // Set Resolution to 1280x720
+    XV_tpg_Set_height(&tpg_inst, HEIGHT);
+    XV_tpg_Set_width(&tpg_inst, WIDTH);
+    // Set Color Space to RGB24
+    // u32 readed = XV_tpg_Get_colorFormat(&tpg_inst);
+    // XV_tpg_Set_colorFormat(&tpg_inst, 0x0);
     
-	UINTPTR Addr1=0x10000000;
-
-    print("VDMA init started\n\r");
+    // Change the pattern to color bar
+    XV_tpg_Set_bckgndId(&tpg_inst, XTPG_BKGND_COLOR_BARS);
     
-    print("Initialize frame buffer data...\n\r");
-    //Disable the data cache to initialize the frame buffer with a blue color
-    Xil_DCacheDisable();
+    // Set Overlay to moving box
+    // Set the size of the box
+    XV_tpg_Set_boxSize(&tpg_inst, 150);
+    XV_tpg_Set_boxColorR(&tpg_inst, 0xff);
+    XV_tpg_Set_boxColorG(&tpg_inst, 0xff);
+    XV_tpg_Set_boxColorB(&tpg_inst, 0xff);
 
-	//Write grey bar pattern in memory
-#ifdef PATTERN_GREY
-    for(int line=0; line < HEIGHT; line++)
-    	{
-    		for(int column=0; column < WIDTH; column++)
-    		{
-
-    			if(column<WIDTH/8)
-    			{
-    				Xil_Out32(Addr1, 0x00e0e0e0);
-    			}
-    			else if(column<2*WIDTH/8)
-    			{
-    				Xil_Out32(Addr1, 0x00c0c0c0);
-    			}
-    			else if(column<3*WIDTH/8)
-    			{
-    				Xil_Out32(Addr1, 0x00a0a0a0);
-    			}
-    			else if(column<4*WIDTH/8)
-    			{
-    				Xil_Out32(Addr1, 0x00808080);
-    			}
-    			else if(column<5*WIDTH/8)
-    			{
-    				Xil_Out32(Addr1, 0x00606060);
-    			}
-    			else if(column<6*WIDTH/8)
-    			{
-    				Xil_Out32(Addr1, 0x00404040);
-    			}
-    			else if(column<7*WIDTH/8)
-    			{
-    				Xil_Out32(Addr1, 0x00202020);
-    			}
-    			else if(column<WIDTH-5)
-    			{
-    				Xil_Out32(Addr1, 0);
-    			}
-    			else
-    			{
-    				Xil_Out32(Addr1, 0);
-    			}
-
-    			Addr1+=4;
-    		}
-
-        }
-#else
-	#ifdef PATTERN_COL_BARS
-    	//Write a color bar pattern in memory
-    	for(int line=0; line < HEIGHT; line++)
-    	{
-    		for(int column=0; column < WIDTH; column++)
-    		{
-
-    			if(column<WIDTH/8)
-    			{
-    				Xil_Out32(Addr1, RED);
-    			}
-    			else if(column<2*WIDTH/8)
-    			{
-    				Xil_Out32(Addr1, GREEN);
-    			}
-    			else if(column<3*WIDTH/8)
-    			{
-    				Xil_Out32(Addr1, BLUE);
-    			}
-    			else if(column<4*WIDTH/8)
-    			{
-    				Xil_Out32(Addr1, RED_HALF);
-    			}
-    			else if(column<5*WIDTH/8)
-    			{
-    				Xil_Out32(Addr1, GREEN_HALF);
-    			}
-    			else if(column<6*WIDTH/8)
-    			{
-    				Xil_Out32(Addr1, BLUE_HALF);
-    			}
-    			else if(column<7*WIDTH/8)
-    			{
-    				Xil_Out32(Addr1, RED_EIGHTS);
-    			}
-    			else if(column<WIDTH-5)
-    			{
-    				Xil_Out32(Addr1, GREEN_EIGHTS);
-    			}
-    			else
-    			{
-    				Xil_Out32(Addr1, 0);
-    			}
-
-    			Addr1+=4;
-    		}
-
-        }
-	#endif
-#endif
-
-	//Re-enable the data cache
-    Xil_DCacheEnable();
-    print("Done\n\r");
-
-	/* Start of VDMA Configuration */
-    xil_printf("VDMA Setup...");
-    /* Configure the Read interface (MM2S)*/
-    // MM2S Control Register
-    Xil_Out32(XPAR_AXI_VDMA_0_BASEADDR + 0x00, 0x8B);
-    // MM2S Start Address 1
-    Xil_Out32(XPAR_AXI_VDMA_0_BASEADDR + 0x5C, 0x10000000);
-    Xil_Out32(XPAR_AXI_VDMA_0_BASEADDR + 0x60, 0x10000000);
-    Xil_Out32(XPAR_AXI_VDMA_0_BASEADDR + 0x64, 0x10000000);
-    Xil_Out32(XPAR_AXI_VDMA_0_BASEADDR + 0x68, 0x10000000);
-
-    // MM2S Frame delay / Stride register
-    Xil_Out32(XPAR_AXI_VDMA_0_BASEADDR + 0x58, WIDTH*4);
-    // MM2S HSIZE register
-    Xil_Out32(XPAR_AXI_VDMA_0_BASEADDR + 0x54, WIDTH*4);
-    // MM2S VSIZE register
-    Xil_Out32(XPAR_AXI_VDMA_0_BASEADDR + 0x50, HEIGHT);
-
-    xil_printf("VDMA started!\r\n");
+    // Set the speed of the box
+    XV_tpg_Set_motionSpeed(&tpg_inst, 5);
+    // Enable the moving box
+    XV_tpg_Set_ovrlayId(&tpg_inst, 1);
+    
+    //Start the TPG
+    XV_tpg_EnableAutoRestart(&tpg_inst);
+    XV_tpg_Start(&tpg_inst);
+   
+    xil_printf("TPG started!\r\n");
     
     while(1);
 
